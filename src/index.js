3e4d575513a4422e5ddd6d0628f8a3fb0201e343
johnnynode <johnnynode@gmail.com>
2018-03-14 10:04:54 +0800

    init project using flux architecture

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './View/TodoApp';
+
+ReactDOM.render(
+  <div>
+    <TodoApp />
+  </div>,
+  document.getElementById('root')
+);
