72af2dcc8c5e1b64d6d391b197928d3e27198c68
johnnynode <johnnynode@gmail.com>
2018-04-26 09:52:53 +0800

    rename 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // 通过 react-redux 来实现store和ui组件的连接
-import store from './store';
+import Store from './store';
 import App from './components/app';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <App />
   </Provider>,
   document.getElementById('root')
